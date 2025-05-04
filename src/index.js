d603cdcbffe3e2ae2b646d024a7a3fa43ecd9011
gulu43 <gulamnabi9th@gmail.com>
2025-05-04 16:56:26 +0530

    still files

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 // require('dotenv').config({path: './env'})
-
+import { app } from './app.js';
 import dotenv from 'dotenv';
 
 import connectDB from "./db/index.js";
+import { app } from './app.js';
 
 dotenv.config({
     path: './env'
  })
-connectDB();
- 
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT_NUMBER || 5210, ()=>{
+        console.log(`Server is running ${process.env.PORT_NUMBER}`);
+    })
+    .on('error',()=>{
+        console.error(` error in connection `);
+        
+    })
+})
+.catch((error) => console.log('mongo connection fails',error) )
