7fff2774278749fa24e14d2f7da138cc0d6cd40f
gulu43 <gulamnabi9th@gmail.com>
2025-05-06 16:50:58 +0530

    added_morefiles

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,31 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs"
+
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) {
+            console.error('cold not find the path');
+        }
+        let response =  await cloudinary.uploader.upload(localFilePath, {
+            resource_type: 'auto'
+        })
+        console.log('file upload'+response.url);
+        return response;
+        
+    } catch (error) {
+        fs.unlinkSync(localFilePath)
+    }
+}
+
+cloudinary.uploader.upload('https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg',
+    { public_id: 'shoes' },
+    function (error, result) {
+        console.log(result);
+    })
