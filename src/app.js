8421409417f0e3f56647485601fea0e17f0efd8d
gulu43 <gulamnabi9th@gmail.com>
2025-05-07 17:09:46 +0530

    some_coding

--- src/app.js
+++ src/app.js
@@ -2,7 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
-const app = express();
+
+let app = express();
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
@@ -16,5 +17,10 @@ app.use(express.urlencoded({extended: true, limit: '16kb'}))
 app.use(express.static('public'))
 //cookie 
 app.use(cookieParser())    
+
+// routes import
+import userRouter from "./routes/user.routes.js";
+
+app.use("/api/v1/users/",userRouter)
 
 export {app};
